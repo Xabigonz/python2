25ee193383e3f799bdd8a37b53b1cd890f0d168b
Dan159159 <113427338+Dan159159@users.noreply.github.com>
2023-02-20 17:19:58 +0100

    Reto2

--- Pag39.py
+++ Pag39.py
@@ -22,23 +22,29 @@ while True:
         break
 
 
-#Con for: y el "RANGE" para poder limitar los intentos:
+# Reto Adivina el numero con FOR
+import random
 
-vidas = range(0,3)
+rnd = random.randint(1, 100)
 
-for numeroAdiv in vidas:
-    pregunta = int(input("Intenta adivinar el número aleatorio del 1 al 100: "))
-    if pregunta < numeroAdiv:
-        
-        print(f"El numero es mayor, ")
-   
-    elif pregunta > numeroAdiv:
+vidas =5
+print("Adivina el numero entre el 1 y 100")
+print(f"Tienes {vidas} intentos para adivinar el número secreto entre 1 y 10.")
+print("------------------------------------------------------------")
 
-        print(f"El numero es menor, ")
+for vida in range(vidas):
+    # Mientra que vida se encuentre en el rango vidas entra
+    res = int(input("Introduce un numero entero : "))
+    if rnd != res:
+        vidas_restantes = vidas - vida
+        if res > rnd:
+            print(f"El número misterioso es menor que {res}. Te quedan {vidas_restantes} intentos.")
 
+        else:
+            print(f"El número misterioso es mayor que {res}. Te quedan {vidas_restantes} intentos.")
     else:
+        print("¡Felicidades, has acertado!")
+        break  # Sale del bucle for
 
-        print (f"El numero es correcto, es {numeroAdiv}")
-        break
-
-# Fin del archio loco
+else:
+    print(f"Lo siento, el número secreto era {rnd}. ¡Mejor suerte la próxima vez!")
